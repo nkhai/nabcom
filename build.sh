4855291c95df83bb794a121455404d817fc509eb
vkn1hc <khai.vonguyen@vn.bosch.com>
2021-06-09 08:18:07 +0700

    change build

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 cd Icom-cart
 docker build -t icom-cart .
 cd ..
-cd Icom-config
-docker build -t icom-config .
-cd ..
 cd Icom-core
 docker build -t icom-core .
 cd ..
@@ -13,7 +10,6 @@ cd ..
 cd Icom-history
 docker build -t icom-history .
 cd ..
-cp -r Icom-xtra-setup/productImg Icom-product/target
 cd Icom-product
 docker build -t icom-product .
 cd ..
@@ -23,6 +19,4 @@ cd ..
 cd Icom-user
 docker build -t icom-user .
 cd ..
-cd Icom-web
-docker build -t icom-web .
-cd ..
+
